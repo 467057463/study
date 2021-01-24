007f0b7c3769b3de6fd7022e4228bdff7bfc88df
m2 <467057463@qq.com>
2021-01-24 10:13:39 +0800

    change extend

--- js/extend/index.js
+++ js/extend/index.js
@@ -1,19 +1,38 @@
-const obj = {
-  a: '1',
-  b: {
-    b1: 'b1',
-    b2: 'b2'
-  }
+function isFunction(obj){
+  return Object.prototype.toString.call(obj) == '[object Function]';
+}
+
+var a = function(){
+
 };
+
+var obj1 = {
+  a: 1,
+  b: {
+    c: 2
+  }
+}
+
+
+var obj2 = {
+  b: {
+    c: [5],
+    // c: 1
+  },
+  d: null
+}
 
 function extend(){
   var deep = false;
   var target = arguments[0] || {};
   let i = 1;
-  if(typeof target !== 'boolean'){
+  if(typeof target == 'boolean'){
     deep = target;
     target = arguments[i]
     i++;
+  }
+  if(typeof target !== 'object' && !isFunction(target)){
+    target = {}
   }
   for(; i < arguments.length; i++){
     const options = arguments[i];
@@ -21,6 +40,7 @@ function extend(){
       for(let key in options){
         let src = target[key];
         let copy = options[key];
+        console.log(src, copy)
         if(deep && copy && typeof copy == 'object'){
           target[key] = extend(deep, src, copy)
         }else if(copy !== undefined){
@@ -32,9 +52,4 @@ function extend(){
   return target;
 }
 
-window.n = extend({}, obj, {
-  a: 2, 
-  b: {
-    b3: 'b3'
-  }
-})
+var d = extend(true, a, obj1, obj2)
