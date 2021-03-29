7560d14a05b6231420e58d105114c6f3014167b2
m2 <467057463@qq.com>
2021-03-29 22:19:53 +0800

    change

---
+++ js/string_match/index.js
@@ -0,0 +1,10 @@
+var str = 'T123his is D1emo. Ha ha mao mao';
+var reg = /[A-Z](\d+)/g;
+
+var rest = str.matchAll(reg);
+
+// var str = 'For more information, see Chapter 3.4.5.1';
+
+// var re = /see (chapter \d+(\.\d)*)/i;
+
+// var ret = str.matchAll(re);
