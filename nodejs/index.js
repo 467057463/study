19194205d9e280b99d82fd39bd07528f045f2cf4
m2 <467057463@qq.com>
2021-07-16 22:29:51 +0800

    init nodejs

---
+++ nodejs/index.js
@@ -0,0 +1,6 @@
+const mod = require('./module1');
+
+console.log(mod)
+
+console.log(__dirname)
+console.log(__filename)
---
+++ nodejs/module1.js
@@ -0,0 +1,11 @@
+exports.methond = function(){
+  return 'hello'
+};
+
+exports.method2 = function(){
+  return 'hi'
+}
+
+module.exports = 'abc';
+console.log(__dirname)
+console.log(__filename)
