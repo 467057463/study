58ec7ae33bbb5afd746dcac83e65d061dbf94019
m2 <467057463@qq.com>
2021-07-23 21:01:54 +0800

    change

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,6 +1,14 @@
-const mod = require('./module1');
+// const mod = require('./module1');
 
-console.log(mod)
+// console.log(mod)
 
-console.log(__dirname)
-console.log(__filename)
+// console.log(__dirname)
+// console.log(__filename)
+
+const src = process.argv.slice(2);
+console.log(src)
+const fs = require('fs');
+
+fs.readFile(src[0], (error, res) => {
+  console.log(res)
+})
