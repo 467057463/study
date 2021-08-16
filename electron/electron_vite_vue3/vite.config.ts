c389d874e235e8c36a76ec76daee67dd25f5321a
m2 <467057463@qq.com>
2021-08-16 22:24:58 +0800

    1. 转移到单独的npm包

--- electron/electron_vite_vue3/vite.config.ts
+++ electron/electron_vite_vue3/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import viteElectron from './vite-plugin-electron';
+import { viteElectron } from 'vite-plugin-electron-builder';
 
 // https://vitejs.dev/config/
 export default defineConfig({
