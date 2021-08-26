f05634a3c282572a0917b430d12b1f29d32d06bc
毛有文 <467057463@qq.com>
2021-08-26 18:43:16 +0800

    完善主进程功能

--- electron/electron_vite_vue3/src/background.ts
+++ electron/electron_vite_vue3/src/background.ts
@@ -5,14 +5,18 @@ import log from 'electron-log';
 // @ts-ignore
 import createProtocol from './createProtocol';
 
-// log.error(__dirname, process.cwd())
+
 
 protocol.registerSchemesAsPrivileged([{ scheme: 'app', privileges: { secure: true, standard: true } }]);
 
+const partition = 'persist:app_session'
+let win: BrowserWindow;
 async function createWindow () {
-  const win = new BrowserWindow({
-    width: 700,
-    height: 1000,
+  win = new BrowserWindow({
+    width: 900,
+    height: 600,
+    frame: false,
+    show: false,
     title: import.meta.env.VITE_NAME + dayjs(),
     webPreferences: {
       nodeIntegration: true,
@@ -20,35 +24,65 @@ async function createWindow () {
       enableRemoteModule: true,
       webSecurity: false,
       preload: path.join(__preload, 'test.js'),
-      partition: 'persist:test_session',
+      partition: partition,
       webviewTag: true,
     }
   })
-  const ses = session.fromPartition('persist:test_webview_session');
-  ses.setProxy({
-    proxyRules:"socks5://127.0.0.1:1080"
-  })
-  createProtocol('app', 'persist:test_session');  
+  // const ses = session.fromPartition('persist:test_webview_session');
+  // ses.setProxy({
+  //   proxyRules:"socks5://127.0.0.1:1080"
+  // })
+  createProtocol('app', partition);  
   if(import.meta.env.DEV_SERVER_URL){
     win.loadURL(import.meta.env.DEV_SERVER_URL)
-    // win.loadURL('http://www.google.com')
+    win.webContents.openDevTools({mode: 'bottom'})
   } else {
     win.loadURL('app://./index.html')
-    // win.loadURL('http://www.baidu.com')
   }
+
+  // 加载完再显示窗口
+  // 以免页面空白
+  win.once('ready-to-show', () => {
+    win.show();
+  });
 }
 
-app.whenReady().then(() => {
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  } else {
+    if (win) win.show();
+  }
+});
+
+app.whenReady().then(async () => {
+  const ses = session.fromPartition(partition);
+  if(import.meta.env.DEV){
+    // 加载本地插件
+    const vueDevToolsPath = `C:\\Users\\admin\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Extensions\\ljjemllljcmogpfapbkkighbhhppjdbg\\6.0.0.15_0`;
+    await ses.loadExtension(vueDevToolsPath);
+  }
   createWindow()
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
 })
 
-
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+// Exit cleanly on request from parent process in development mode.
+if (import.meta.env.DEV) {
+  if (process.platform === 'win32') {
+    process.on('message', (data) => {
+      if (data === 'graceful-exit') {
+        app.quit();
+      }
+    });
+  } else {
+    process.on('SIGTERM', () => {
+      app.quit();
+    });
+  }
+}
 
