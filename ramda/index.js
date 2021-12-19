3769d654430940a1c3fdbc5009672ddac3e3afff
m2 <467057463@qq.com>
2021-12-19 09:34:07 +0800

    change

--- ramda/index.js
+++ ramda/index.js
@@ -6,5 +6,35 @@ const replace = curry((waht, replacement, str) => str.replace(waht, replacement)
 
 const filter = curry((f, ary) => ary.filter(f));
 
-const map = curry((f, ary) => ary.map(f));
+// const map = curry((f, ary) => ary.map(f));
+const map = curry((f, any_functor_at_all) => any_functor_at_all.map(f))
 
+const concat = curry((val, str) => str.concat(val))
+
+function Container (x){
+  this.__value = x;
+}
+
+Container.of = function(x){
+  return new Container(x)
+}
+
+Container.prototype.map = function (f){
+  return Container.of(f(this.__value))
+}
+
+function Maybe (x){
+  this.__value = x;
+}
+
+Maybe.of = function(x){
+  return new Maybe(x)
+}
+
+Maybe.prototype.isNothing = function (){
+  return this.__value === null || this.__value === undefined;
+}
+
+Maybe.prototype.map = function (f){
+  return this.isNothing() ? Maybe.of(null) : Maybe.of(f(this.__value));
+}
--- vue3/vite-vue3/src/App.vue
+++ vue3/vite-vue3/src/App.vue
@@ -1,18 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+<input v-model="name"/>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-})
+<script lang="ts" setup>
+import { watch, ref, computed, watchEffect, reactive,  nextTick, readonly, isProxy, isReactive, isReadonly, toRaw, markRaw, toRef, toRefs, getCurrentInstance } from 'vue';
+const obj = reactive({name: 'mm'})
+// const name = toRef(obj, 'name')
+const {name} = toRefs(obj)
+console.log(name)
+console.log(getCurrentInstance())
 </script>
 
 <style>
