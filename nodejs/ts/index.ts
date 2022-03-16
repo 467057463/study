869b7999716f7f26ce7fc7cc142203dc7232ed4e
m2 <467057463@qq.com>
2022-03-16 22:45:06 +0800

    change

--- nodejs/ts/index.ts
+++ nodejs/ts/index.ts
@@ -34,3 +34,36 @@ function filter2<Type, Func extends (arg: Type) => boolean>(
 
 filter1([1], (i) => !!i)
 filter2([1], (i)=> i> 0)
+
+function firstElement<T>(arr: T[]): T | undefined{
+  return arr[0]
+}
+
+const s = firstElement([1, 2])
+
+function map<Input, Output>(arr: Input[], func: (arg: Input) => Output): Output[] {
+  return arr.map(func);
+}
+
+const parsed = map(['1'], (n) => parseInt(n))
+
+function longest<Type extends {length: number}>(a: Type, b: Type){
+  if(a.length > b.length){
+    return a;
+  }
+  return b
+}
+
+longest('a', 'bbb')
+longest("111111", [1111])
+longest(2, 1)
+
+
+function minimumLength<Type extends {length: number}>(obj: Type, minimum: number): Type{
+  if(obj.length >= minimum){
+    return obj;
+  }
+  return {
+    length: minimum
+  }
+}
