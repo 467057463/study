65ac9fe3f38e9535d4ac1c9f6014aca5c1875351
毛有文 <467057463@qq.com>
2022-03-16 18:39:04 +0800

    学习泛型函数

--- nodejs/ts/index.ts
+++ nodejs/ts/index.ts
@@ -1,2 +1,36 @@
-const sizes = ['large', 'default', 'small'] as const
-export type ComponentSize = typeof sizes[0]
+function combine<T>(arr1: T[], arr2: T[]): T[]{
+  return arr1.concat(arr2)
+}
+
+const arr = combine<string | number>([1], ['1'])
+
+type types = 'all' | 'pined' | 'unread';
+type PropType<O> = {
+  [K in types]?: O
+}
+
+type InforItem = {
+  name: string;
+  age: number
+}
+
+const props: PropType<InforItem> = {
+  all: {
+    name: '11',
+    age: 11
+  }
+}
+
+function filter1<Type>(arr: Type[], func: (arg: Type) => boolean): Type[] {
+  return arr.filter(func);
+}
+ 
+function filter2<Type, Func extends (arg: Type) => boolean>(
+  arr: Type[],
+  func: Func
+): Type[] {
+  return arr.filter(func);
+}
+
+filter1([1], (i) => !!i)
+filter2([1], (i)=> i> 0)
