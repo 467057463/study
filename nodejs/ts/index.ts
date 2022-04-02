96b6e91ae589e424d4452e2161cd17d315eccb06
毛有文 <467057463@qq.com>
2022-04-02 16:44:33 +0800

    change

--- nodejs/ts/index.ts
+++ nodejs/ts/index.ts
@@ -1,69 +1,9 @@
-function combine<T>(arr1: T[], arr2: T[]): T[]{
-  return arr1.concat(arr2)
+type BuildLength = {
+  length: number
 }
 
-const arr = combine<string | number>([1], ['1'])
+const buildProps = <O extends {
+  [K in keyof O]: O[K] extends BuildLength ? O[K] : [O[K]]
+}>(props: O) => props;
 
-type types = 'all' | 'pined' | 'unread';
-type PropType<O> = {
-  [K in types]?: O
-}
-
-type InforItem = {
-  name: string;
-  age: number
-}
-
-const props: PropType<InforItem> = {
-  all: {
-    name: '11',
-    age: 11
-  }
-}
-
-function filter1<Type>(arr: Type[], func: (arg: Type) => boolean): Type[] {
-  return arr.filter(func);
-}
- 
-function filter2<Type, Func extends (arg: Type) => boolean>(
-  arr: Type[],
-  func: Func
-): Type[] {
-  return arr.filter(func);
-}
-
-filter1([1], (i) => !!i)
-filter2([1], (i)=> i> 0)
-
-function firstElement<T>(arr: T[]): T | undefined{
-  return arr[0]
-}
-
-const s = firstElement([1, 2])
-
-function map<Input, Output>(arr: Input[], func: (arg: Input) => Output): Output[] {
-  return arr.map(func);
-}
-
-const parsed = map(['1'], (n) => parseInt(n))
-
-function longest<Type extends {length: number}>(a: Type, b: Type){
-  if(a.length > b.length){
-    return a;
-  }
-  return b
-}
-
-longest('a', 'bbb')
-longest("111111", [1111])
-longest(2, 1)
-
-
-function minimumLength<Type extends {length: number}>(obj: Type, minimum: number): Type{
-  if(obj.length >= minimum){
-    return obj;
-  }
-  return {
-    length: minimum
-  }
-}
+buildProps({length: '1'})
---
+++ react-native/hello_world_ts/App.tsx
@@ -0,0 +1,43 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Image, ScrollView, Text} from 'react-native';
+
+const logo = {
+  uri: 'https://reactnative.dev/img/tiny_logo.png',
+  width: 64,
+  height: 64,
+};
+
+const App = () => {
+  return (
+    <ScrollView>
+      <Text style={{fontSize: 96}}>Scroll</Text>
+      <Image source={logo} />
+      <Image source={logo} />
+      <Image source={logo} />
+      <Image source={logo} />
+      <Image source={logo} />
+      <Image source={logo} />
+      <Image source={logo} />
+      <Image source={logo} />
+      <Text style={{fontSize: 96}}>hello world</Text>
+      <Image source={logo} />
+      <Image source={logo} />
+      <Image source={logo} />
+      <Image source={logo} />
+      <Image source={logo} />
+      <Image source={logo} />
+    </ScrollView>
+  );
+};
+
+export default App;
