0bdeb2f9704f9ef0e301b37dcad6fd6897426ef8
毛有文 <467057463@qq.com>
2022-04-15 18:32:33 +0800

    change

--- react-native/test/App.tsx
+++ react-native/test/App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import logo from './assets/logo.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!!!!</Text>
+      <Image source={logo} style={{width: 306, height: 159}}/>
+      <Text style={{color: 'yellow', fontSize: 24}}>Open up App.tsx to start working on your app!!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
