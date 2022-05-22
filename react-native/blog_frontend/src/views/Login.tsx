db864ff040f26c88e5a3a237d497dad9f0501b68
m2 <467057463@qq.com>
2022-05-22 22:34:05 +0800

    study elements

--- react-native/blog_frontend/src/views/Login.tsx
+++ react-native/blog_frontend/src/views/Login.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native-web';
+import React, { useState } from 'react';
+import { View, StyleSheet, Text, Image } from 'react-native-web';
 import {Button} from '@rneui/themed';
+import { Divider } from "@rneui/themed";
+import { Badge, Icon, withBadge  } from "@rneui/themed";
+import { BottomSheet } from "@rneui/themed";
+import { Card } from "@rneui/themed";
+
+const BadgedIcon = withBadge(15)(Icon);
 
 export default function Login({navigation}){
+  const [isVisible, setIsVisible] = useState(false);
+
   return(
     <View style={styles.view}>
       <Button 
@@ -11,10 +19,33 @@ export default function Login({navigation}){
           screen: 'Settings',
         })
         }/>
+      {/* @ts-ignore */}
+      <BadgedIcon type="ionicon" name="ios-chatbubbles" />
+      <Text>divider</Text>
+      <Divider color='red' width={2}/>
       <Button
         title="忘记密码"
         onPress={() => navigation.navigate('ForgetPassword')}
       />
+      <Button
+        title="Open Bottom Sheet"
+        onPress={() => setIsVisible(true)}
+      />
+      {/* @ts-ignore */}
+      <BottomSheet modalProps={{}} isVisible={isVisible} onBackdropPress={() => setIsVisible(false)}>
+        <Text>ABCDEFG</Text>
+      </BottomSheet>
+      {/* @ts-ignore */}
+      <Card>
+        <Card.Title>CARD TITLE</Card.Title>
+        <Card.Divider/>
+        <Card.Image
+          style={styles.image}
+          resizeMode="cover"
+          source={{ uri: 'https://randomuser.me/api/portraits/men/41.jpg' }}
+        />
+        <Text>Test</Text>
+      </Card>
     </View>
   )
 }
@@ -24,5 +55,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: 30,
+    height: 30,
+    marginRight: 10,
   },
 });
