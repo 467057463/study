6eeef888b388b23d90711d81a483e0cdf04901c3
m2 <467057463@qq.com>
2023-01-13 23:36:42 +0800

    change

---
+++ cpp/hello_c/index.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    using namespace std;
+
+    int number;
+
+    number = 11122333;
+    cout << "number is " << endl;
+    cout << number;
+    cout << endl;
+
+    return 0;
+}
---
+++ cpp/test/test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by 毛有文 on 2023/1/13.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
