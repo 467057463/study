7ab64ce47fe431014f07420a457d2bfeca64d44b
m2 <467057463@qq.com>
2023-04-24 11:16:01 +0800

    test

--- nodejs/esbuild/index.ts
+++ nodejs/esbuild/index.ts
@@ -1,7 +1,45 @@
 import { build } from 'esbuild';
+import { accessSync, constants, existsSync, readdirSync, statSync } from 'fs';
+import { spawnSync } from 'child_process';
+import { join } from 'path';
+
+// function getDIr(path: string){  
+//   function walk(p: string){
+//     if(!existsSync(p)){
+//       return
+//     }
+//     console.log('p', p)
+//     return readdirSync(p).reduce((prev: Record<string, any>, item) => {
+//       const np = join(p, item)
+//       if(statSync(np).isDirectory()){
+//         prev.list = walk(np)
+//       } else {
+//         prev[item] = item;
+//       }
+//       return prev;
+//     }, {})
+//   }
+
+//   return walk(path)
+// }
+
+// const res = getDIr('./node_modules');
+// console.log(res);
+
+
+// spawnSync("npm help", {
+//   shell: true,
+//   stdio: "inherit"
+// })
+// process.exit
 
 build({
   entryPoints: ['react/app.jsx'],
   bundle: true,
-  outfile: 'out.js'
+  loader: {
+    '.png': 'dataurl'
+  },
+  outdir: 'dist',
+  // outfile: 'index.js',
+  // external: ['./react/utils/index.ts']
 })
