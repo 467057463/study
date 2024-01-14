ff5559e18a240900aea3a96e2d7abef557898257
m2 <467057463@qq.com>
2024-01-14 09:09:32 +0800

    vite
    init vite project

---
+++ cpp/test/leigod_test/leigod_test/leigod_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  leigod_testApp.swift
+//  leigod_test
+//
+//  Created by 毛有文 on 2023/10/9.
+//
+
+import SwiftUI
+
+@main
+struct leigod_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
