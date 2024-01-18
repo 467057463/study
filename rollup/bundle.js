daf315c4a32a24378608ac5ca20719e00a050868
m2 <467057463@qq.com>
2024-01-19 07:31:19 +0800

    init rollup

---
+++ rollup/bundle.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const version = '1.0.0';
+
+// (async function(){
+//   await delay()
+//   console.log('after delay')
+// })()
+console.log('hello rollup', version);
--- rollup/src/main.js
+++ rollup/src/main.js
@@ -1,1 +1,7 @@
-console.log('hello rollup')
+import * as utils from "./utils/index.js";
+
+// (async function(){
+//   await delay()
+//   console.log('after delay')
+// })()
+console.log('hello rollup', utils.version)
