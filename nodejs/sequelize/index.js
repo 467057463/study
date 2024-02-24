281585994fdeaa1ddf648344d2c1f8c25d8f22b7
m2 <467057463@qq.com>
2024-02-24 08:07:00 +0800

    style

--- nodejs/sequelize/index.js
+++ nodejs/sequelize/index.js
@@ -1,20 +1,52 @@
-const { Sequelize, DataTypes } = require("sequelize")
+const { Sequelize, DataTypes, Model } = require("sequelize")
 
 
-const sequelize = new Sequelize('learning_development', 'root', 'nandudu_', {
+const sequelize = new Sequelize('sequelize_development', 'root', 'nandudu_', {
   host: 'localhost',
   dialect: 'mysql'
 });
 
-const Worker = sequelize.define('Worker', {
+// 方法一：使用define
+const User = sequelize.define('user', {
   firstName: {
-    type: DataTypes.STRING,
+    type: DataTypes.TEXT,
     allowNull: false
   },
 
   lastName: {
-    type: DataTypes.STRING,
+    type: DataTypes.TEXT,
+    allowNull: false
   }
-})
+});
 
-console.log(Worker, sequelize.models.Worker)
+// 方法二：扩展
+// class User extends Model {}
+
+// User.init({
+//   firstName: {
+//     type: DataTypes.STRING,
+//     allowNull: false
+//   },
+
+//   lastName: {
+//     type: DataTypes.STRING,
+//   }
+// }, { sequelize });
+
+( async function (){
+  await sequelize.sync()
+  const user = await User.create({
+    firstName: 'm',
+    lastName: 'm'
+  })
+
+  const users = await User.findAll({
+    attributes: ['firstName', ['lastName', 'l_n']],
+    where: {
+      firstName: 'cw'
+    }
+  })
+  console.log(users)
+}())
+
+
