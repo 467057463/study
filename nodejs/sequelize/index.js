de5e8fa636f5fafc638edc974da8114f88c7c8f3
m2 <467057463@qq.com>
2024-03-14 22:28:10 +0800

    feat:
    1. init nuxt

--- nodejs/sequelize/index.js
+++ nodejs/sequelize/index.js
@@ -19,6 +19,7 @@ const User = sequelize.define('user', {
   }
 });
 
+
 // 方法二：扩展
 // class User extends Model {}
 
@@ -33,20 +34,55 @@ const User = sequelize.define('user', {
 //   }
 // }, { sequelize });
 
+const Firend = sequelize.define('firend', {
+  work: {
+    type: DataTypes.TEXT,
+  }
+})
+
+User.belongsToMany(Firend, { through: 'userFirends'})
+Firend.belongsToMany(User, { through: 'userFirends'});
+
 ( async function (){
   await sequelize.sync()
-  const user = await User.create({
-    firstName: 'm',
-    lastName: 'm'
-  })
 
-  const users = await User.findAll({
-    attributes: ['firstName', ['lastName', 'l_n']],
-    where: {
-      firstName: 'cw'
-    }
+  // // 添加
+  // const user = await User.create({
+  //   firstName: 'm',
+  //   lastName: 'm'
+  // })
+
+  // // 更新
+  // await User.update({firstName: 'cw'}, {
+  //   where: {
+  //     id: 1
+  //   }
+  // })
+
+  // const users = await User.findAll({
+  //   attributes: ['firstName', ['lastName', 'l_n'], 'id'],
+  //   where: {
+  //     firstName: 'm'
+  //   },
+  //   order: [
+  //     ["id", "DESC"]
+  //   ],
+  //   // group: "firstName"
+  // })
+  // console.log(users)
+
+  const firend = await Firend.create({
+    work: 'proger',
+    userId: 1
   })
-  console.log(users)
+  const user = await User.findByPk(1)
+  await user.addFirend(firend)
+  const userF = await User.findByPk(1, {
+    include: Firend
+  })
+  console.log(userF)
+  // const f = await user.getFirend()
+  // console.log(f)
 }())
 
 
