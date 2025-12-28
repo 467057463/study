5ffa7bd5b8632d5b532809201f44d2729d0a535e
m2 <467057463@qq.com>
2025-12-28 21:54:10 +0800

    fix

---
+++ cpp/carrots/carrots.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+  using namespace std;
+
+  int carrots;
+
+  carrots = 25;
+  cout << "carrots is";
+  cout << carrots;
+  cout << endl;
+  carrots = carrots + 10;
+  cout << "now" << carrots << endl;
+}
---
+++ cpp/carrots/getinfo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+  using namespace std;
+
+  int carrots;
+
+  cout << "places input" << endl;
+  cin >> carrots;
+  cout << carrots << "input" << endl;
+  cin.get();
+  cin.get();
+  return 0;
+}
